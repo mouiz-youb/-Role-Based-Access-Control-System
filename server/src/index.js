cf202f0ac0634fb0450a7dcbfa26f7e3e75c813b
abdelmouiz <a_youb@estin.dz>
2025-10-30 05:19:27 -1100

    project setup is done

---
+++ server/src/index.js
@@ -0,0 +1,21 @@
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv';
+
+dotenv.config();
+const app = express();
+const PORT = process.env.PORT || 5000;
+app.use(cors());
+app.use(express.json());
+// starter of server 
+const Start = ()=>{
+    app.listen(PORT ,()=>{
+        console.log(`Server is running on port ${PORT}`);
+    })
+}
+
+Start();
+
+
+
+
